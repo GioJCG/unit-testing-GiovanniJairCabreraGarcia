64fbdd830abe8141148f8cdf7e0aa64fc67ad2ab
GiovaJCG <zS22004370@estudiantes.uv.mx>
2024-10-16 19:05:08 -0600

    add factorial branch #2

---
+++ src/app/factorial/factorial.ts
@@ -0,0 +1,27 @@
+export function factorial(x: number) {
+
+    var numero = 0
+
+    if( x < 0)
+         numero = 0
+
+    if(x == 0)
+         numero  = 1
+
+    if(x == 2)
+     numero = x
+
+    if(x == 3)
+     numero = x + x
+
+    if(x == 4)
+     numero = 24
+
+    if(x == 5)
+     numero = 120
+
+    if(x > 15)
+     numero 
+
+    return numero
+}

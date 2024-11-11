e33dd82ea39dfa1ce2e13f7eaf68158defe374fd
GiovaJCG <zS22004370@estudiantes.uv.mx>
2024-11-10 22:44:43 -0600

    update branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MediaComponent } from './media/media.component';
+import { StddevComponent } from './stddev/stddev.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'media', component: MediaComponent },
+    { path: 'stddev', component: StddevComponent },
+];
